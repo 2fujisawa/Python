d8671cda82fa324e02a9af093f0a9124184619a2
Linus Fujisawa <92174981+2fujisawa@users.noreply.github.com>
2025-01-17 14:39:08 -0800

    Inhertiance

--- OOP.py
+++ OOP.py
@@ -13,15 +13,42 @@ class Person: #when creating an class you also have to create object which you m
         self.is_student = is_student
 
     #Instance Method
-    def greet(self):
+    def greet(self): #the self is needed in the parameter so the method knows which object to work with
         return (f"Hello my name is {self.name} and I am {self.age} years old. I am a student: {self.is_student}")
     
+'''
+Inhertiance - Mechanism to define a new class based on an existing class
+    - allows you to create a new class that inherits attributes and methods from a existing class(subclasses and superclasses)
+'''
+
+class Student(Person): #Student class inherits from Person class
+    def __init__(self,name, age, is_student, student_id):
+        self.sudent_id = student_id
+        super().__init__(name, age, is_student) #super() method is used here to call in the constuctor of the person class so you dont have to redefine the instance variables
+
+    def greet_as_student(self):
+        return(f"{self.greet()} My student id is {self.sudent_id}") #greet has () since it is a method
+
+
+
+
 
 def main():
 # Object - Instance of the class
-    #Creating object of the class 
+    #Creating object of the class, instantiating the class
     firstperson = Person(name = "Linus", age = 22, is_student = True) #You need to declare all of the attributes in the object
     print(firstperson.greet())
+
+    secondperson = Person(name = "John", age = 27, is_student = False)
+    print(secondperson.greet())
+
+    #Accessing class variable - so we can see that class variables are used to store data that is shared by all instances of the class as to instance variables that are unique to each instance of the class
+    print(f"{firstperson.name}'s gender is {firstperson.gender}") 
+    print(f"{secondperson.name}'s gender is {secondperson.gender}")
+
+    firststudent = Student(name = "Jacob", age = 22, is_student = True, student_id = 1234)
+    print(firststudent.greet_as_student())
+
 
 if __name__ =="__main__":
     main()
